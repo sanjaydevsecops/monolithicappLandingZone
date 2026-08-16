240c52d743f14df41870907625a55092fd34200c
Sanjay DevSecOps <devops.sanjay.kumar@gmail.com>
2026-08-16 07:07:49 +0530

    made changes

--- environments/preprod/provider.tf
+++ environments/preprod/provider.tf
@@ -7,14 +7,14 @@ terraform {
     }
   }
 
-  backend "azurerm" {
-    use_oidc             = true
-    use_azuread_auth     = true
-    client_id            = "3ac542a8-7044-47e8-97e2-3bbb73fbfe3a"
-    storage_account_name = "skstorage2"
-    container_name       = "tfstate"
-    key                  = "preprod.terraform.tfstate"
-  }
+  # backend "azurerm" {
+  #   use_oidc             = true
+  #   use_azuread_auth     = true
+  #   client_id            = "3ac542a8-7044-47e8-97e2-3bbb73fbfe3a"
+  #   storage_account_name = "skstorage2"
+  #   container_name       = "tfstate"
+  #   key                  = "preprod.terraform.tfstate"
+  # }
 }
 
 
