203f24ddcefbd67c10fb3d81f4dbc2f0dae57c4b
Sanjay DevSecOps <devops.sanjay.kumar@gmail.com>
2026-08-16 06:27:16 +0530

    made changes

--- environments/preprod/provider.tf
+++ environments/preprod/provider.tf
@@ -6,7 +6,17 @@ terraform {
       version = "4.80.0"
     }
   }
+
+  backend "azurerm" {
+    use_oidc             = true
+    use_azuread_auth     = true
+    client_id            = "3ac542a8-7044-47e8-97e2-3bbb73fbfe3a"
+    storage_account_name = "skstorage2"
+    container_name       = "tfstate"
+    key                  = "preprod.terraform.tfstate"
+  }
 }
+
 
 provider "azurerm" {
   features {}
